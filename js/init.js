1451d9cfb4a0cb3beff64da90b44f60c9597b0bc
Qhub <qdaman90@gmail.com>
2019-05-31 10:54:11 -0500

    First commit

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+  $(function(){
+    
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.scrollspy').scrollSpy();
+    $('.modal').modal();
+    $('.tabs').tabs({swipeable: true});
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
